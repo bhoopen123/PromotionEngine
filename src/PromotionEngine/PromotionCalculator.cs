6cb1f2cdb0badec103a62043779333598dcdf2a0
bhoopen123 <34786750+bhoopen123@users.noreply.github.com>
2020-09-02 22:44:46 +0530

    defined structure for applying promotions / discounts

--- src/PromotionEngine/PromotionCalculator.cs
+++ src/PromotionEngine/PromotionCalculator.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿
+using System.Collections.Generic;
 
 namespace PromotionEngine
 {
     public class PromotionCalculator
     {
-        public decimal Calculate(char[] products)
+        private List<IDiscount> _discountCalculators = null;
+
+        public PromotionCalculator()
         {
-            // TODO: Discount and applying promotions will be implemented here.
-            return decimal.Zero;
+            _discountCalculators = new List<IDiscount>();
+
+            // adding all types of discount calculator
+            _discountCalculators.Add(new QuantityDiscount());
+            _discountCalculators.Add(new GroupDiscount());
+            
+            // any new dicsountCalculator
+        }
+
+        public decimal Calculate(char[] productsInCart)
+        {
+            decimal discountedPrice = decimal.Zero;
+            
+            // applying discount or promotions on products
+            foreach (var discountCalculator in _discountCalculators)
+            {
+                discountedPrice += discountCalculator.ApplyDiscount(productsInCart);
+            }
+
+            return discountedPrice;
         }
     }
 }
